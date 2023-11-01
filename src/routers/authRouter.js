3747d95b7df9f774cf7c1f745ffe008a3d1b43b1
mutty.schwarz <mutty320@gmail.com>
2023-11-01 18:08:42 +0200

    added error message for incorrect name or key

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -7,15 +7,19 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const authRouter = express.Router();
 authRouter.route("/signIn").post(urlencodedParser, (req, res) => {
   const { user_name, api_secret_key } = req.body;
-  
+
   if (
     user_name == process.env.USER_NAME &&
     api_secret_key == process.env.OPENAI_SECRET_KEY
   ) {
     res.render("submitText");
   } else {
-    console.log("wrong name or key");
-    res.render("index");
+    const error = {
+      message:
+        "Unauthorized, you provided Incorrect username or Incorrect key",
+    };
+
+    res.render("index", { error });
   }
 });
 
